079845c5b08f43d61c47ff0ba8bfa2e48021e720
Kevin Maguire II <k.w.maguire.ii@gmail.com>
2020-03-03 14:01:10 -0500

    New Layout, font sizes and icons instead of star rating system

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var btn = $('#to-top-button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $(window).scrollTop(0);
+});
+
