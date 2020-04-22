fe3b27393e7bf1db605ba4f895aaa53b7e6db4c1
Kevin Maguire II <k.w.maguire.ii@gmail.com>
2020-04-22 17:23:59 -0400

    fresh start

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Pdf from "./resume/Resume_Kevin_Maguire.pdf"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href= {Pdf}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Site is currently under development.  Please see my resume.
         </a>
       </header>
     </div>
