9de1a4929f14d49d39e112c229dbcbe88c01bd91
Kevin Maguire <k.w.maguire.ii@gmail.com>
2020-04-26 14:29:32 -0400

    Logo and Basic Components

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
 import Pdf from "./resume/Resume_Kevin_Maguire.pdf"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href= {Pdf}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Site is currently under development.  Please see my resume.
-        </a>
-      </header>
+    <div>
+      <NavBar/>
     </div>
   );
 }
